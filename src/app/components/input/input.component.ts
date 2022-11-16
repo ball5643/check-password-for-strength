7c1dc2e9e96a5f4358cb745c2033775a8db06cd2
Balls <nicolajkorchikov54@gmail.com>
2022-11-16 17:45:29 +0200

    commit

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-  color1: string = "";
-  color2: string = "";
-  color3: string = "";
+  color1: string = "grey";
+  color2: string = "grey";
+  color3: string = "grey";
 
   constructor() { }
 
@@ -19,10 +19,16 @@ export class InputComponent implements OnInit {
     const inputValue = event.target.value;
     const inputLength = inputValue.length;
 
-    const letters = /^[a-zA-Z]+$/
-    const numbers = /[0-9]/;
-    const symbols = /[$-/:-?{-~!"^_@`\[\]]/;
-    const onlyLettersDigitsSymbols = /^[a-zA-Z0-9~_&*%@$]+$/
+    //-------------FIRST CASE----------------------
+    const letters = /^(?=.*[A-Za-z])[A-Za-z]{8,}$/
+    const numbers = /^(?=.*\d)[\d]{8,}$/
+    const symbols = /^(?=.*[@$!%*#?&])[@$!%*#?&]{8,}$/
+    //-------------SECOND CASE---------------------
+    const lettersDigits = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+    const lettersSymbols = /^(?=.*[A-Za-z])(?=.*[@$!%*#?&])[A-Za-z@$!%*#?&]{8,}$/
+    const symbolsDigits = /^(?=.*\d)(?=.*[@$!%*#?&])[\d@$!%*#?&]{8,}$/
+    //------------------------THIRD CASE------------------------------
+    const lettersDigitsSymbols = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
 
     console.log(inputValue);
     if (inputLength === 0) {
@@ -30,15 +36,37 @@ export class InputComponent implements OnInit {
       this.color2 = "grey";
       this.color3 = "grey";
     }
-    else if (inputLength < 8) {
-      this.color1 = "red";
-      this.color2 = "red";
-      this.color3 = "red";
-    }
-    else if(letters.test(inputValue) || numbers.test(inputValue)) {
+
+    else if (
+      inputValue.match(letters) ||
+      inputValue.match(numbers) ||
+      inputValue.match(symbols))
+    {
       this.color1 = "red";
       this.color2 = "grey";
       this.color3 = "grey";
+    }
+
+    else if (
+      inputValue.match(lettersDigits) ||
+      inputValue.match(lettersSymbols) ||
+      inputValue.match(symbolsDigits))
+    {
+      this.color1 = "yellow";
+      this.color2 = "yellow";
+      this.color3 = "grey";
+    }
+
+    else if (inputValue.match(lettersDigitsSymbols)) {
+      this.color1 = "lime";
+      this.color2 = "lime";
+      this.color3 = "lime";
+    }
+
+    else if(inputLength < 8 && inputLength > 0) {
+      this.color1 = "red";
+      this.color2 = "red";
+      this.color3 = "red";
     }
   }
 }
