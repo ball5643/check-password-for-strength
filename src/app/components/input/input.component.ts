426f05f8e5d57bae6b19f6a00d8c5703c506a8ca
Balls <nicolajkorchikov54@gmail.com>
2022-11-18 01:33:03 +0200

    commit

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -3,53 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
-  easyColor: string = "grey";
-  mediumColor: string = "grey";
-  strongColor: string = "grey";
+  easyColor: string = 'grey';
+  mediumColor: string = 'grey';
+  strongColor: string = 'grey';
 
   constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onKey(event: any) {
     const inputValue = event.target.value;
     const inputLength = inputValue.length;
 
-    //-------------FIRST CASE----------------------
     const easyPassword = /^[a-z]+$|^\d+$|^[@$!%*#?&]+$/i;
-    //-------------SECOND CASE---------------------
     const mediumPassword = /^[a-z\d]+$|^[\d@$!%*#?&]+$|^[a-z@$!%*#?&]+$/i;
-    //------------------------THIRD CASE------------------------------
     const strongPassword = /^[A-Za-z\d@$!%*#?&]+$/i;
 
-  if (inputLength === 0) {
-      this.easyColor = "grey";
-      this.mediumColor = "grey";
-      this.strongColor = "grey";
+    if (inputLength === 0) {
+      this.easyColor = 'grey';
+      this.mediumColor = 'grey';
+      this.strongColor = 'grey';
+    } else if (inputValue.length < 8) {
+      this.easyColor = 'red';
+      this.mediumColor = 'red';
+      this.strongColor = 'red';
+    } else if (inputValue.match(easyPassword)) {
+      this.easyColor = 'red';
+      this.mediumColor = 'grey';
+      this.strongColor = 'grey';
+    } else if (inputValue.match(mediumPassword)) {
+      this.easyColor = 'yellow';
+      this.mediumColor = 'yellow';
+      this.strongColor = 'grey';
+    } else if (inputValue.match(strongPassword)) {
+      this.easyColor = 'lime';
+      this.mediumColor = 'lime';
+      this.strongColor = 'lime';
+    }
   }
-  else if (inputValue.length < 8) {
-    this.easyColor = "red";
-    this.mediumColor = "red";
-    this.strongColor = "red";
-  }
-  else if (inputValue.match(easyPassword)) {
-    this.easyColor = "red";
-    this.mediumColor = "grey";
-    this.strongColor = "grey";
-  }
-  else if (inputValue.match(mediumPassword)) {
-    this.easyColor = "yellow";
-    this.mediumColor = "yellow";
-    this.strongColor = "grey";
-  }
-  else if (inputValue.match(strongPassword)) {
-    this.easyColor = "lime";
-    this.mediumColor = "lime";
-    this.strongColor = "lime";
-  }
- }
 }
