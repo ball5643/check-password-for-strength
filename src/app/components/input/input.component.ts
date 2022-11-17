45a430e824a13c3ee31cb6c13911727d262bcddd
Balls <nicolajkorchikov54@gmail.com>
2022-11-18 00:54:35 +0200

    commit

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -6,11 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input.component.css']
 })
 export class InputComponent implements OnInit {
-  color1: string = "grey";
-  color2: string = "grey";
-  color3: string = "grey";
+  easyColor: string = "grey";
+  mediumColor: string = "grey";
+  strongColor: string = "grey";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
@@ -20,53 +20,36 @@ export class InputComponent implements OnInit {
     const inputLength = inputValue.length;
 
     //-------------FIRST CASE----------------------
-    const letters = /^(?=.*[A-Za-z])[A-Za-z]{8,}$/
-    const numbers = /^(?=.*\d)[\d]{8,}$/
-    const symbols = /^(?=.*[@$!%*#?&])[@$!%*#?&]{8,}$/
+    const easyPassword = /^[a-z]+$|^\d+$|^[@$!%*#?&]+$/i;
     //-------------SECOND CASE---------------------
-    const lettersDigits = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
-    const lettersSymbols = /^(?=.*[A-Za-z])(?=.*[@$!%*#?&])[A-Za-z@$!%*#?&]{8,}$/
-    const symbolsDigits = /^(?=.*\d)(?=.*[@$!%*#?&])[\d@$!%*#?&]{8,}$/
+    const mediumPassword = /^[a-z\d]+$|^[\d@$!%*#?&]+$|^[a-z@$!%*#?&]+$/i;
     //------------------------THIRD CASE------------------------------
-    const lettersDigitsSymbols = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/
+    const strongPassword = /^[A-Za-z\d@$!%*#?&]+$/i;
 
-    console.log(inputValue);
-    if (inputLength === 0) {
-      this.color1 = "grey";
-      this.color2 = "grey";
-      this.color3 = "grey";
-    }
-
-    else if (
-      inputValue.match(letters) ||
-      inputValue.match(numbers) ||
-      inputValue.match(symbols))
-    {
-      this.color1 = "red";
-      this.color2 = "grey";
-      this.color3 = "grey";
-    }
-
-    else if (
-      inputValue.match(lettersDigits) ||
-      inputValue.match(lettersSymbols) ||
-      inputValue.match(symbolsDigits))
-    {
-      this.color1 = "yellow";
-      this.color2 = "yellow";
-      this.color3 = "grey";
-    }
-
-    else if (inputValue.match(lettersDigitsSymbols)) {
-      this.color1 = "lime";
-      this.color2 = "lime";
-      this.color3 = "lime";
-    }
-
-    else if(inputLength < 8 && inputLength > 0) {
-      this.color1 = "red";
-      this.color2 = "red";
-      this.color3 = "red";
-    }
+  if (inputLength === 0) {
+      this.easyColor = "grey";
+      this.mediumColor = "grey";
+      this.strongColor = "grey";
   }
+  else if (inputValue.length < 8) {
+    this.easyColor = "red";
+    this.mediumColor = "red";
+    this.strongColor = "red";
+  }
+  else if (inputValue.match(easyPassword)) {
+    this.easyColor = "red";
+    this.mediumColor = "grey";
+    this.strongColor = "grey";
+  }
+  else if (inputValue.match(mediumPassword)) {
+    this.easyColor = "yellow";
+    this.mediumColor = "yellow";
+    this.strongColor = "grey";
+  }
+  else if (inputValue.match(strongPassword)) {
+    this.easyColor = "lime";
+    this.mediumColor = "lime";
+    this.strongColor = "lime";
+  }
+ }
 }
